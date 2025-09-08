6ba87b5f97983d0a3eef81e6cd89188963df253f
Ericlesrobsom <137913541+Ericlesrobsom@users.noreply.github.com>
2025-09-08 04:28:56 -0300

    Corrige import inválido: troca teste-custom por testeCustom

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,101 +1,54 @@
 import Layout from "./Layout.jsx";
-
 import Dashboard from "./Dashboard";
-
 import Products from "./Products";
-
 import Orders from "./Orders";
-
 import Deliverers from "./Deliverers";
-
 import Store from "./Store";
-
 import MyOrders from "./MyOrders";
-
 import Settings from "./Settings";
-
 import Banners from "./Banners";
-
 import Home from "./Home";
-
-import teste-custom from "./teste-custom";
-
+import testeCustom from "./teste-custom";
 import Categories from "./Categories";
-
 import DeliveryMethods from "./DeliveryMethods";
-
 import Advertisements from "./Advertisements";
-
 import Questions from "./Questions";
-
 import Coupons from "./Coupons";
-
 import Courses from "./Courses";
-
 import CoursePlayer from "./CoursePlayer";
-
 import CourseEditor from "./CourseEditor";
-
 import Links from "./Links";
-
 import SocialLinks from "./SocialLinks";
-
 import GlobalOffers from "./GlobalOffers";
-
 import MyProfile from "./MyProfile";
-
 import Affiliates from "./Affiliates";
 
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 const PAGES = {
-    
     Dashboard: Dashboard,
-    
     Products: Products,
-    
     Orders: Orders,
-    
     Deliverers: Deliverers,
-    
     Store: Store,
-    
     MyOrders: MyOrders,
-    
     Settings: Settings,
-    
     Banners: Banners,
-    
     Home: Home,
-    
-    teste-custom: teste-custom,
-    
+    testeCustom: testeCustom,
     Categories: Categories,
-    
     DeliveryMethods: DeliveryMethods,
-    
     Advertisements: Advertisements,
-    
     Questions: Questions,
-    
     Coupons: Coupons,
-    
     Courses: Courses,
-    
     CoursePlayer: CoursePlayer,
-    
     CourseEditor: CourseEditor,
-    
     Links: Links,
-    
     SocialLinks: SocialLinks,
-    
     GlobalOffers: GlobalOffers,
-    
     MyProfile: MyProfile,
-    
     Affiliates: Affiliates,
-    
 }
 
 function _getCurrentPage(url) {
@@ -119,56 +72,30 @@ function PagesContent() {
     return (
         <Layout currentPageName={currentPage}>
             <Routes>            
-                
-                    <Route path="/" element={<Dashboard />} />
-                
-                
+                <Route path="/" element={<Dashboard />} />
                 <Route path="/Dashboard" element={<Dashboard />} />
-                
                 <Route path="/Products" element={<Products />} />
-                
                 <Route path="/Orders" element={<Orders />} />
-                
                 <Route path="/Deliverers" element={<Deliverers />} />
-                
                 <Route path="/Store" element={<Store />} />
-                
                 <Route path="/MyOrders" element={<MyOrders />} />
-                
                 <Route path="/Settings" element={<Settings />} />
-                
                 <Route path="/Banners" element={<Banners />} />
-                
                 <Route path="/Home" element={<Home />} />
-                
-                <Route path="/teste-custom" element={<teste-custom />} />
-                
+                <Route path="/teste-custom" element={<testeCustom />} />
                 <Route path="/Categories" element={<Categories />} />
-                
                 <Route path="/DeliveryMethods" element={<DeliveryMethods />} />
-                
                 <Route path="/Advertisements" element={<Advertisements />} />
-                
                 <Route path="/Questions" element={<Questions />} />
-                
                 <Route path="/Coupons" element={<Coupons />} />
-                
                 <Route path="/Courses" element={<Courses />} />
-                
                 <Route path="/CoursePlayer" element={<CoursePlayer />} />
-                
                 <Route path="/CourseEditor" element={<CourseEditor />} />
-                
                 <Route path="/Links" element={<Links />} />
-                
                 <Route path="/SocialLinks" element={<SocialLinks />} />
-                
                 <Route path="/GlobalOffers" element={<GlobalOffers />} />
-                
                 <Route path="/MyProfile" element={<MyProfile />} />
-                
                 <Route path="/Affiliates" element={<Affiliates />} />
-                
             </Routes>
         </Layout>
     );
